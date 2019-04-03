c656f7e00c8f7651c6fac79614b5861b90c47be9
Adrian Schatzmann <adrian.schatzmann@butos.ch>
2019-04-03 23:53:33 +0200

    DummyPasswordHasher implemented

---
+++ Website/Code/DummyPasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace Website.Code
+{
+    public class DummyPasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return password;
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword)
+        {
+            return string.Equals(hashedPassword, providedPassword) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+        }
+    }
+}
